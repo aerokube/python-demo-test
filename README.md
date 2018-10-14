b1041576b546faeb56d9870203acc71d83962546
Ivan Krutov <vania-pooh@vania-pooh.com>
2018-10-14 11:15:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Selenium Python Demo Test
+
+This repository contains a simple test to check whether your Selenium installation works. To run it:
+
+1) Install dependencies:
+```
+$ sudo pip install selenium
+```
+
+2) Run the test:
+```
+python test.py
+```
---
+++ test.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+capabilities = {
+    "browserName": "chrome",
+    "version": "69.0",
+    "platform": "LINUX"
+}
+
+driver = webdriver.Remote(
+    command_executor='http://localhost:4444/wd/hub',
+    desired_capabilities=capabilities
+)
+
+try:
+    print 'Opening the page...'
+    driver.get('http://duckduckgo.com/')
+
+    print 'Typing search request...'
+    search_input = driver.find_element_by_css_selector(
+        "input[id='search_form_input_homepage']")
+    search_input.send_keys("selenium", Keys.ENTER)
+
+    print 'Taking screenshot...'
+    driver.get_screenshot_as_file(driver.session_id + '.png')
+finally:
+    driver.quit()
